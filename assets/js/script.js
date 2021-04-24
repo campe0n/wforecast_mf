dd2b2192655a7302d9aedf023fac5b4ba5223cbe
Melvin Finn <melvinrf@Outlook.com>
2021-04-23 21:03:58 -0500

    was able to make the data update with a different city, but lost my uv index data in the process

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,8 @@ var day = dayjs().format('(D/MM/YY)');
 var search = document.querySelector('input').value.trim();
 console.log(day);
 function getApi() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=38.883530&lon=-94.818237&units=imperial&appid=35f55e88a2361c526b9d9dfd2f2e3074'
-    //var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + search +'&appid=35f55e88a2361c526b9d9dfd2f2e3074'
+    var search = document.querySelector('input').value.trim();
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&q=' + search +'&appid=35f55e88a2361c526b9d9dfd2f2e3074'
 
     fetch(requestUrl)
     .then(function(response) {
@@ -13,44 +13,59 @@ function getApi() {
     })
     .then(function(data) {
         console.log(data)
-        document.getElementById('temp').innerHTML = "Temp: "+ data.current.temp+"F";
-        document.getElementById('wind').innerHTML = "Wind  "+data.current.wind_speed+" MPH";
-        document.getElementById('humidity').innerHTML = "Humidity:  "+data.current.humidity+" %";
-        document.getElementById('uvi').innerHTML = "UV Index:  "+data.current.uvi;   
+        document.getElementById('temp').innerHTML = "Temp: "+ data.main.temp+"F";
+        document.getElementById('wind').innerHTML = "Wind  "+data.wind.speed+" MPH";
+        document.getElementById('humidity').innerHTML = "Humidity:  "+data.main.humidity+" %";
+    })
+}
 
-        document.getElementById('t1').innerHTML = "Temp: "+ JSON.stringify(data.daily[0].temp.day);
+function get5day(){
+    var search = document.querySelector('input').value.trim();
+    //var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=38.883530&lon=-94.818237&units=imperial&appid=35f55e88a2361c526b9d9dfd2f2e3074'
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='+search+'&units=imperial&appid=35f55e88a2361c526b9d9dfd2f2e3074'
+
+    fetch(requestUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data)
+        //document.getElementById('uvi').innerHTML = "UV Index:  "+data.current.uvi;   
+
+        //target every 8th item
+        document.getElementById('t1').innerHTML = "Temp: "+ data.list[0].main.temp+' F';
         document.getElementById('t1').setAttribute('style', 'color:white;');
-        document.getElementById('w1').innerHTML = "Wind: "+ JSON.stringify(data.daily[0].wind_speed);
+        document.getElementById('w1').innerHTML = "Wind: "+ data.list[0].wind.speed+' MPH';
         document.getElementById('w1').setAttribute('style', 'color:white');
-        document.getElementById('h1').innerHTML = "Humidity: "+JSON.stringify(data.daily[0].humidity);
+        document.getElementById('h1').innerHTML = "Humidity: "+ data.list[0].main.humidity+' %';
         document.getElementById('h1').setAttribute('style', 'color: white');
 
-        document.getElementById('t2').innerHTML = "Temp: "+ JSON.stringify(data.daily[1].temp.day);
+        document.getElementById('t2').innerHTML = "Temp: "+ data.list[7].main.temp+' F';
         document.getElementById('t2').setAttribute('style', 'color:white;');
-        document.getElementById('w2').innerHTML = "Wind: "+ JSON.stringify(data.daily[1].wind_speed);
+        document.getElementById('w2').innerHTML = "Wind: "+ data.list[7].wind.speed+' MPH';
         document.getElementById('w2').setAttribute('style', 'color:white');
-        document.getElementById('h2').innerHTML = "Humidity: "+JSON.stringify(data.daily[1].humidity);
+        document.getElementById('h2').innerHTML = "Humidity: "+ data.list[7].main.humidity+' %';
         document.getElementById('h2').setAttribute('style', 'color: white');
 
-        document.getElementById('t3').innerHTML = "Temp: "+ JSON.stringify(data.daily[2].temp.day);
+        document.getElementById('t3').innerHTML = "Temp: "+ data.list[15].main.temp+' F';
         document.getElementById('t3').setAttribute('style', 'color:white;');
-        document.getElementById('w3').innerHTML = "Wind: "+ JSON.stringify(data.daily[2].wind_speed);
+        document.getElementById('w3').innerHTML = "Wind: "+ data.list[15].wind.speed+' MPH';
         document.getElementById('w3').setAttribute('style', 'color:white');
-        document.getElementById('h3').innerHTML = "Humidity: "+JSON.stringify(data.daily[2].humidity);
+        document.getElementById('h3').innerHTML = "Humidity: "+ data.list[15].main.humidity+' %';
         document.getElementById('h3').setAttribute('style', 'color: white');
 
-        document.getElementById('t4').innerHTML = "Temp: "+ JSON.stringify(data.daily[3].temp.day);
+        document.getElementById('t4').innerHTML = "Temp: "+ data.list[23].main.temp+' F';
         document.getElementById('t4').setAttribute('style', 'color:white;');
-        document.getElementById('w4').innerHTML = "Wind: "+ JSON.stringify(data.daily[3].wind_speed);
+        document.getElementById('w4').innerHTML = "Wind: "+ data.list[23].wind.speed+' MPH';
         document.getElementById('w4').setAttribute('style', 'color:white');
-        document.getElementById('h4').innerHTML = "Humidity: "+JSON.stringify(data.daily[3].humidity);
+        document.getElementById('h4').innerHTML = "Humidity: "+ data.list[23].main.humidity+' %';
         document.getElementById('h4').setAttribute('style', 'color: white');
 
-        document.getElementById('t5').innerHTML = "Temp: "+ JSON.stringify(data.daily[4].temp.day);
+        document.getElementById('t5').innerHTML = "Temp: "+ data.list[35].main.temp+' F';
         document.getElementById('t5').setAttribute('style', 'color:white;');
-        document.getElementById('w5').innerHTML = "Wind: "+ JSON.stringify(data.daily[4].wind_speed);
+        document.getElementById('w5').innerHTML = "Wind: "+ data.list[35].wind.speed+' MPH';
         document.getElementById('w5').setAttribute('style', 'color:white');
-        document.getElementById('h5').innerHTML = "Humidity: "+JSON.stringify(data.daily[4].humidity);
+        document.getElementById('h5').innerHTML = "Humidity: "+ data.list[35].main.humidity+' %';
         document.getElementById('h5').setAttribute('style', 'color: white');
     })
 }
@@ -67,6 +82,7 @@ function formSubmitHandler() {
         document.querySelector('h3').innerHTML = search + '        ' + day;
         createButtons();
         getApi();
+        get5day();
     }
 }
 
